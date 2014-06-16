49eb4c2247bac9ab997b3a9b94ed9f38b3dfe328
Sylvester Keil <sylvester@keil.or.at>
2014-06-16 19:21:00 +0200

    add pick and omit

--- test/utils.js
+++ test/utils.js
@@ -18,4 +18,34 @@ describe('Zotero.utils', function () {
     });
   });
 
+  describe('#pick', function () {
+    var pick = utils.pick;
+
+    it('picks the passed-in keys from the object', function () {
+      pick({}).should.be.empty;
+      pick({ foo: 1, bar: 2 }, 'foo').should.have.properties({ foo: 1 });
+      pick({ foo: 1, bar: 2 }, 'bar').should.have.properties({ bar: 2 });
+      pick({ foo: 1, bar: 2 }, 'bar', 'foo').should.have.properties({ foo: 1, bar: 2 });
+
+      pick({ foo: 1, bar: 2 }, ['foo']).should.have.properties({ foo: 1 });
+      pick({ foo: 1, bar: 2 }, ['bar']).should.have.properties({ bar: 2 });
+      pick({ foo: 1, bar: 2 }, ['bar', 'foo']).should.have.properties({ foo: 1, bar: 2 });
+    });
+  });
+
+  describe('#omit', function () {
+    var omit = utils.omit;
+
+    it('picks all but the passed-in keys from the object', function () {
+      omit({}).should.be.empty;
+      omit({ foo: 1, bar: 2 }, 'foo').should.have.properties({ bar: 2 });
+      omit({ foo: 1, bar: 2 }, 'bar').should.have.properties({ foo: 1 });
+      omit({ foo: 1, bar: 2 }, 'bar', 'foo').should.be.empty;
+      omit({ foo: 1, bar: 2 }).should.have.properties({ foo: 1, bar: 2 });
+      omit({ foo: 1, bar: 2 }, 'baz').should.have.properties({ foo: 1, bar: 2 });
+
+      omit({ foo: 1, bar: 2 }, ['bar', 'foo']).should.be.empty;
+      omit({ foo: 1, bar: 2 }, ['baz', 'foo']).should.have.properties({ bar: 2 });
+    });
+  });
 });
