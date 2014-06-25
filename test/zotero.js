7f2c127b897e639acf856402aed9fcc0b77a724c
Sylvester Keil <sylvester@keil.or.at>
2014-06-25 17:12:55 +0200

    add top-level tests

--- test/zotero.js
+++ test/zotero.js
@@ -1,4 +1,5 @@
 var zotero = require('..'),
+  debug = require('debug'),
   sinon = require('sinon');
 
 describe('zotero', function () {
@@ -7,4 +8,24 @@ describe('zotero', function () {
   it('.Client is defined', function () {
     zotero.Client.should.be.a.Function;
   });
+
+  it('.Library is defined', function () {
+    zotero.Library.should.be.a.Function;
+  });
+
+  it('calling it returns a library instance', function () {
+    zotero().should.be.instanceof(zotero.Library);
+  });
+
+  describe('.print', function () {
+    it('is a function', function () { zotero.print.should.be.a.Function; });
+
+    it('can be called with error and message parameters', function () {
+      sinon.stub(console, 'log');
+      zotero.print(sinon.stub({}), sinon.stub({ code: 42 }));
+      console.log.called.should.be.true;
+      console.log.restore();
+    });
+  });
+
 });
