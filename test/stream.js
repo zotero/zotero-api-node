07ddc0adfbfc21bb02cb2c70d69760009862a9f7
Sylvester Keil <sylvester@keil.or.at>
2015-02-04 17:26:02 +0100

    add stream test scaffold

---
+++ test/stream.js
@@ -0,0 +1,11 @@
+'use strict';
+
+var Stream = require('../lib/stream');
+//var Message = require('../lib/message'),
+//var nock = require('nock');
+
+describe('Zotero.Stream', function () {
+
+  it('is a constructor', function () { Stream.should.be.a.Function; });
+
+});
