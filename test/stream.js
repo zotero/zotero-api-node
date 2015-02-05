9b1bf2fa36b66211b6a816dbd9fa07754bc77906
Sylvester Keil <sylvester@keil.or.at>
2015-02-05 18:01:56 +0100

    add stream event tests

--- test/stream.js
+++ test/stream.js
@@ -19,7 +19,11 @@ describe('Zotero.Stream', function () {
 
           'retry: 1000\n\n' +
           'event: connected\n' +
-          'data: {"connectionId":"foobar"}\n\n',
+          'data: {"connectionId":"foobar"}\n\n' +
+          'event: topicUpdated\n' +
+          'data: {"topic":"foo","version":23}\n\n' +
+          'event: topicUpdated\n' +
+          'data: {"topic":"bar","version":24}\n\n',
 
           {
             'Content-Type': 'text/event-stream'
@@ -36,6 +40,37 @@ describe('Zotero.Stream', function () {
         this.should.be.instanceof(Stream);
         this.connection.should.eql('foobar');
 
+        done();
+      });
+    });
+
+    it('listeners can be added', function (done) {
+      var s = new Stream();
+      var called = 0;
+
+      s.on('topicUpdated', function () { ++called; });
+
+      // Error called when stream terminates!
+      s.on('error', function () {
+        called.should.eql(2);
+        done();
+      });
+    });
+
+    it('listeners can be removed', function (done) {
+      var s = new Stream();
+      var called = 0;
+
+      function listener() {
+        s.removeListener('topicUpdated', listener);
+        called++;
+      }
+
+      s.on('topicUpdated', listener);
+
+      // Error called when stream terminates!
+      s.on('error', function () {
+        called.should.eql(1);
         done();
       });
     });
